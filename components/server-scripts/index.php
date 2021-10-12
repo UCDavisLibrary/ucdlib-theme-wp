07e387bb73e30bdc82a25bd33bc839cbee810683
spelkey-ucd <spelkey@ucdavis.edu>
2021-10-12 13:53:18 -0400

    #42

--- components/server-scripts/index.php
+++ components/server-scripts/index.php
@@ -1,4 +1,6 @@
 <?php
 require_once(__DIR__ . "/transformations.php");
 require_once(__DIR__ . "/blocks.php");
+require_once(__DIR__ . "/meta_data.php");
+new UCDThemeMetaData();
 ?>
---
+++ components/server-scripts/meta_data.php
@@ -0,0 +1,38 @@
+<?php
+
+class UCDThemeMetaData {
+  function __construct(){
+
+    // Assign a UCD theme color to a category
+    add_action('category_add_form_fields', array($this, 'display_taxonomy_color'), 10, 2);
+    add_action('category_edit_form_fields', array($this, 'display_taxonomy_color'), 10, 2);
+    add_action('edited_category', array($this, 'save_taxonomy_color'), 10, 2);
+    add_action('create_category', array($this, 'save_taxonomy_color'), 10, 2);
+  }
+
+  /**
+   * Renders input box for specifying a theme color for a taxonomy term.
+   */
+  function display_taxonomy_color($term){
+    $context = array(
+      'current_filter' => current_filter(),
+      'term' => $term
+    );
+    if ( is_object($term) ) {
+      $context['selected_color'] = get_term_meta($term->term_id, 'theme-color', true);
+    }
+    Timber::render( "@ucd/admin/category_colors.twig", $context );
+  }
+
+  /**
+   * Saves theme color for a taxonomy term
+   */
+  function save_taxonomy_color($term_id){
+    if (!isset($_POST['theme-color'])) {
+      return;
+    }
+    update_term_meta($term_id, 'theme-color', sanitize_text_field($_POST['theme-color']));
+  }
+}
+
+?>
