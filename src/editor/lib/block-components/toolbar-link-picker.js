876acff0a464c675ecded47313b5c77ef2288423
spelkey-ucd <spelkey@ucdavis.edu>
2022-05-16 10:24:30 -0400

    #90

--- src/editor/lib/block-components/toolbar-link-picker.js
+++ src/editor/lib/block-components/toolbar-link-picker.js
@@ -26,7 +26,14 @@ function ToolbarLinkPicker({
     onChange( {url: ''});
   }
 
+  const stripProtocol = (v) => {
+    v = v.replace('http://', '').replace('https://');
+    return v;
+  }
   const _onChange = (v) => {
+    if ( ( isEmail || isPhone ) && v && v.url ) {
+      v.url = stripProtocol(v.url);
+    }
     if ( isEmail && v && v.url && !v.url.startsWith('mailto:') ) {
       v.url = 'mailto:' + v.url;
     } else if ( isPhone && v && v.url && !v.url.startsWith('tel:')) {
@@ -45,7 +52,7 @@ function ToolbarLinkPicker({
     if ( !isEmail && hasValue && !hasMailTo ) {
       setIsPhone(false);
       if ( hasTel ) value.url = value.url.slice(4);
-      value.url = 'mailto:' + value.url;
+      value.url = stripProtocol('mailto:' + value.url);
       onChange(value);
     }
     setIsEmail(!isEmail);
@@ -61,7 +68,7 @@ function ToolbarLinkPicker({
     if ( !isPhone && hasValue && !hasTel ) {
       setIsEmail(false);
       if ( hasMailTo ) value.url = value.url.slice(7);
-      value.url = 'tel:' + value.url;
+      value.url = stripProtocol('tel:' + value.url);
       onChange(value);
     }
     setIsPhone(!isPhone);
