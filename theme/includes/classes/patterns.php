3584311c342b9f0e409cc774fc862d9557bf1955
Dusty Cartwright <dcartwright@ucdavis.edu>
2022-06-22 00:11:14 +0000

    core text styles pattern #140

--- theme/includes/classes/patterns.php
+++ theme/includes/classes/patterns.php
@@ -35,6 +35,17 @@ class UCDLibThemePatterns {
         'keywords' => ['contact', 'library'],
       ]
     );
+
+    register_block_pattern(
+      "$this->slug/coretextstyles",
+      [
+        'title' => 'Core Text Styles',
+        'content' => $this->markupCoreTextStyles(),
+        'description' => 'Core Text Styles',
+        'categories' => [$this->slug],
+        'keywords' => ['core', 'text', 'style'],
+      ]
+    );
   }
 
   public function markupContactUs(){
@@ -73,6 +84,24 @@ class UCDLibThemePatterns {
     <div class=\"wp-block-group\"><!-- wp:ucd-theme/heading {\"content\":\"Department or Audience\",\"level\":3,\"classSuffix\":\"h6\"} /-->
     <!-- wp:ucd-theme/prefixed-icon-link {\"icon\":\"ucd-public:fa-envelope\"} /-->
     <!-- wp:ucd-theme/prefixed-icon-link {\"icon\":\"ucd-public:fa-phone\"} /--></div>
+    <!-- /wp:group -->
+    ";
+  }
+
+  public function markupCoreTextStyles(){
+    return "
+    <!-- wp:group -->
+    <div class=\"wp-block-group\">
+    <!-- wp:ucd-theme/heading {\"content\":\"Primary Headings\"} /-->
+    <!-- wp:paragraph -->
+    <p>Normal text may be <strong>bolded</strong>, <em>italicized</em> and <a href=\"https://sandbox.library.ucdavis.edu/\" data-type=\"page\" data-id=\"111\">linked</a> but otherwise never underlined. Underlines are reserved for links.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:list {\"className\":\"list\u002d\u002darrow\"} -->
+    <ul class=\"list--arrow\"><li>Preferred List Style (Gold Arrow)</li><li>Exception: Use \"Default\" list style in FAQ blocks due to the light blue background</li></ul>
+    <!-- /wp:list -->
+    <!-- wp:ucd-theme/heading {\"content\":\"2nd Level Headings\",\"level\":3,\"classSuffix\":\"highlight\"} /-->
+    <!-- wp:ucd-theme/heading {\"content\":\"3rd Level Headings\",\"level\":4,\"classSuffix\":\"h5\"} /-->
+    <!-- wp:ucd-theme/heading {\"content\":\"4th Level Headings\",\"level\":5,\"classSuffix\":\"h6\"} /--></div>
     <!-- /wp:group -->
     ";
   }
