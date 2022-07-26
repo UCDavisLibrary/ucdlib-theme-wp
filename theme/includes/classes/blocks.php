e27e936be5365fcb85cc15e8cd441e1f4adcd72c
Sabrina Baggett <sabaggett@ucdavis.edu>
2022-07-26 15:20:38 -0700

    change icons displayed in block.php

--- theme/includes/classes/blocks.php
+++ theme/includes/classes/blocks.php
@@ -21,13 +21,6 @@ class UCDThemeBlocks extends UCDThemeBlockRenderer {
     $this->iconsUsed = [
       'ucd-public:fa-star',
       'ucd-public:fa-circle-chevron-right',
-      'ucd-public:fa-circle-exclamation',
-      'ucd-public:facebook',
-      'ucd-public:twitter',
-      'ucd-public:instagram',
-      'ucd-public:youtube',
-      'ucd-public:linkedin',
-      'ucd-public:fa-circle-chevron-right',
       'ucd-public:fa-circle-exclamation'
     ];
 
