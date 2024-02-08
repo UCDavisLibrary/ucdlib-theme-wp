91980d35dd6231944e0fd4bb14b1f52b2e5784d4
spelkey-ucd <spelkey@ucdavis.edu>
2024-02-08 11:20:39 -0500

    #279

--- src/editor/lib/blocks/ucd-theme-priority-links/edit.js
+++ src/editor/lib/blocks/ucd-theme-priority-links/edit.js
@@ -1,13 +1,22 @@
-import { html } from "../../utils";
-import { useBlockProps, 
-  useInnerBlocksProps, 
+import { html, UCDIcons } from "../../utils";
+import {
+  useBlockProps,
+  BlockControls,
+  useInnerBlocksProps,
 } from '@wordpress/block-editor';
+import { ToolbarButton } from "@wordpress/components";
 import { Fragment } from "@wordpress/element";
 
 export default ( props ) => {
+  const { attributes, setAttributes } = props;
+
   const ALLOWED_BLOCKS = [ 'ucd-theme/priority-links-item' ];
+  const classes = [ "priority-links" ];
+  if ( attributes.boxedOffset ){
+    classes.push("priority-links--boxed-offset");
+  }
   const blockProps = useBlockProps( {
-	  className: "priority-links",
+	  className: classes.join(' ')
 	} );
   const defaultTemplate = [
     ['ucd-theme/priority-links-item']
@@ -21,6 +30,14 @@ export default ( props ) => {
 
   return html`
     <${Fragment}>
+      <${BlockControls} group="block">
+        <${ToolbarButton}
+          icon=${UCDIcons.renderPublic("fa-arrow-up-from-bracket")}
+          onClick=${ () => {setAttributes({'boxedOffset': !attributes.boxedOffset})}}
+          isPressed=${attributes.boxedOffset}
+          label="Toggle 'Boxed Offset' Modifier"
+        />
+      </${BlockControls}>
       <div ...${ innerBlocksProps } >
       </div>
     </${Fragment}>
